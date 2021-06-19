1e493029c2f7ef9e51fab9842c777ab41a9e2e5f
馗顺先生 <2727144006@qq.com>
2021-06-19 21:21:53 +0800

    完善RLE压缩程序
    - 1、完善了文件的打开功能
    - 2、实现了压缩算法
    TODO : 搞清楚为啥文件操作出了问题

--- Code/基于RLE算法的压缩程序/RLEcompress.c
+++ Code/基于RLE算法的压缩程序/RLEcompress.c
@@ -13,9 +13,11 @@
 #define MAX_DIR 1024
 #define MAX_FILE_NAME 512
 #define MAX_FILE_SUFFIX 16
-#define SUFFIX_NAME "ys"
-#define SUCCESS 1
-#define FAILURE 0
+#define COUNTER_MAX 255
+#define SIZE_CNT 1
+#define SUFFIX_NAME "rlec"
+#define OPEN_SUCCESS 1
+#define OPEN_FAILURE 0
 
 typedef struct fileInfo
 {
@@ -24,25 +26,28 @@ typedef struct fileInfo
     char suffix[MAX_FILE_SUFFIX];
     FILE *fp;
 }fileInfo;
-typedef status int;
+typedef int status;
+typedef unsigned char buffElem;
+typedef unsigned char countElem;
 
 #endif
 
 #if 1   // The declarations of functions.
 
 void putMenu(void);
-fileInfo *initFile(void);
+fileInfo *initFIleInfo(void);
+void getFileLoc(char dir_Destination[], char * directory, char * fileName, const char * fileSuffix);
 status getFileInfo(fileInfo * fileInfoPoint);
-void destoryFile(fileInfo * fileInfoPoint);
+void destoryFileInfo(fileInfo * fileInfoPoint);
 void compressFile(fileInfo * file);
-void decompressFile(fileInfo * file);
+void deCompressFile(fileInfo * file);
 
 #endif
 
 int main(void)
 {
     int choice = 0;
-    fileInfo opeateFile = initFile();
+    fileInfo *opeateFile = initFIleInfo();
     while (1)
     {
         putMenu();
@@ -60,7 +65,7 @@ int main(void)
                     else
                     {
                         printf("[ERROR]: Directory or file name input error!\n");
-                        printf("Reinput(1) or quit input(0) ?")
+                        printf("Reinput(1) or quit input(0) ?");
                         scanf("%d", &choice);
                         if (!choice)
                             break;
@@ -70,7 +75,7 @@ int main(void)
             case 2:
                 while (1)
                 {
-                    if (getFileInfo(opeateFile) && strcmp(opeateFile.suffix, SUFFIX_NAME) == 0)
+                    if (getFileInfo(opeateFile) && strcmp(opeateFile->suffix, SUFFIX_NAME) == 0)
                     {
                         deCompressFile(opeateFile);
                         break;
@@ -78,14 +83,14 @@ int main(void)
                     else
                     {
                         printf("[ERROR]: Directory or file name input error!\n");
-                        printf("Reinput(1) or quit input(0) ?")
+                        printf("Reinput(1) or quit input(0) ?");
                         scanf("%d", &choice);
                         if (!choice)
                             break;
                     }
                 }
             default:
-                destoryFile(opeateFile);
+                destoryFileInfo(opeateFile);
                 return 0;
         }
     }
@@ -101,24 +106,98 @@ void putMenu(void)
     printf("Please input your choice : ");
 }
 
-fileInfo *initFile(void)
+fileInfo *initFIleInfo(void)
 {
     fileInfo *t = (fileInfo *)malloc(sizeof(fileInfo));
     memset(t->directory, '\0', sizeof t->directory);
     memset(t->name, '\0', sizeof t->name);
-    memset(t->suffix, '\0', t->suffix);
+    memset(t->suffix, '\0', sizeof t->suffix);
     t->fp = NULL;
     return t;
 }
 
 status getFileInfo(fileInfo *fileInfoPoint)
 {
+    int dotLoc = -1;
+    char directory[MAX_DIR + MAX_FILE_NAME + MAX_FILE_SUFFIX + 2];
     char dir[MAX_DIR], fileName[MAX_FILE_NAME + MAX_FILE_SUFFIX];
+    printf("Please input the directory of goal file  : \n");
+    scanf("%s", dir);
+    strcpy(fileInfoPoint->directory, dir);
+    printf("Please input the name of goal name  :\n");
+    scanf("%s", fileName);
+    for (int i = strlen(fileName) - 1; i >= 0; i--)
+        if (fileName[i] == '.')
+        {
+            dotLoc = i;
+            fileName[dotLoc] = '\0';
+            break;
+        }
+    if (dotLoc == -1)
+        strcpy(fileInfoPoint->name, fileName);
+    else
+    {
+        strcpy(fileInfoPoint->name, fileName);
+        fileName[dotLoc] = '.';
+        strcpy(fileInfoPoint->suffix, fileName + dotLoc + 1);
+    }
+    
+    getFileLoc(directory, dir, fileInfoPoint->name, fileInfoPoint->suffix);
+    if ((fileInfoPoint->fp = fopen(directory, "rb")) == NULL)
+        return OPEN_FAILURE;
+    else
+    {
+        fclose(fileInfoPoint->fp);
+        return OPEN_SUCCESS;
+    }
 }
 
-void destory(fileInfo * fileInfoPoint)
+void destoryFileInfo(fileInfo * fileInfoPoint)
 {
-    fileInfoPoint.fp = NULL;
+    fileInfoPoint->fp = NULL;
     free(fileInfoPoint);
     fileInfoPoint = NULL;
+}
+
+void compressFile(fileInfo * file)
+{
+    char directory[MAX_DIR + MAX_FILE_NAME + MAX_FILE_SUFFIX + 2];
+    getFileLoc(directory, file->directory, file->name, SUFFIX_NAME);
+    FILE *read_fp = file->fp;
+    FILE *write_fp = fopen(directory, "wb");
+    buffElem predata, data;
+    countElem counter = 1;
+    fread(&predata, sizeof(buffElem), SIZE_CNT, read_fp);
+    printf("%d %d\n", predata, counter);
+    while ((fread(&data, sizeof(buffElem), SIZE_CNT, read_fp)) == SIZE_CNT)
+    {
+        if ((data ^ predata) == 0 && counter < COUNTER_MAX)
+            counter++;
+        else
+        {
+            printf("%d %d\n", predata, counter);
+            fwrite(&predata, sizeof(buffElem), SIZE_CNT, write_fp);
+            fwrite(&counter, sizeof(countElem), SIZE_CNT, write_fp);
+            predata = data;
+            counter = 1;
+        }
+    }
+    printf("%d %d\n", predata, counter);
+    fwrite(&predata, sizeof(buffElem), SIZE_CNT, write_fp);
+    fwrite(&counter, sizeof(countElem), SIZE_CNT, write_fp);
+    fclose(file->fp);
+    fclose(write_fp);
+}
+
+void deCompressFile(fileInfo * file)
+{}
+
+void getFileLoc(char dir_Destination[], char * directory, char * fileName, const char * fileSuffix)
+{
+    memset(dir_Destination, '\0', MAX_DIR + MAX_FILE_NAME + MAX_FILE_SUFFIX + 2);
+    strcpy(dir_Destination, directory);
+    strcat(dir_Destination, "\\");
+    strcat(dir_Destination, fileName);
+    strcat(dir_Destination, ".");
+    strcat(dir_Destination, fileSuffix);
 }
